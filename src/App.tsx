519fd678060ce6b667d16ef2dffe3bb84743fc5c
mtselaove <mtsealove0927@gmail.com>
2022-10-27 22:23:29 +0900

    테스트

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const onClick = () => {
+        //@ts-ignore
+        window.location = 'intent:https://m.naver.com#Intent;end';
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClick}>
+        외부 링크 열기
+      </button>
     </div>
   );
 }
